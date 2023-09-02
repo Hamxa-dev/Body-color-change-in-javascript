67c98728f238281fea42cc3f9abf2b35832eeb98
Hafiz Hamza <hafizhamxa8@gmail.com>
2023-09-02 17:20:28 +0500

    first commit

---
+++ apps.js
@@ -0,0 +1,37 @@
+document.getElementById("Black");
+Black.addEventListener("click",Black );
+function Black(){
+    document.body.style.backgroundColor=("#000000");//  BlAcK
+}
+
+
+
+document.getElementById("yellow");
+yellow.addEventListener("click",yellow)
+function yellow(){
+    document.body.style.backgroundColor=("#ffff00"); // yellow
+}
+
+
+
+document.getElementById("Red");
+Red.addEventListener("click",Red)
+function Red(){
+    document.body.style.backgroundColor=("#ff0000")// Red
+}
+
+
+
+document.getElementById("Green");
+Green.addEventListener("click",Green);
+function Green(){
+    document.body.style.backgroundColor=("#008000")//green
+}
+
+
+
+document.getElementById("blue");//blue
+blue.addEventListener("click",blue);
+function blue(){
+    document.body.style.backgroundColor=("#0000ff")
+}
